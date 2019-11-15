f92673343c2dc74d9a575f4efef0782b66eadad5
Sever Banesiu <sever@uber.com>
2019-11-14 23:43:16 -0800

    More goodies

--- at28c256/at28c256.ino
+++ at28c256/at28c256.ino
@@ -41,8 +41,10 @@
 
 */
 
-#define BAUD_RATE 57600
+#include <string.h>
+#include <limits.h>
 
+#define BAUD_RATE 230400
 #define PIN_CE 41
 #define PIN_WE 40
 #define PIN_OE 39
@@ -136,10 +138,10 @@ static bool poll(byte data) {
   bool inCycle = true;
   chipEnable();
   while (inCycle && counter > 0) {
-    readEnable(); // TODO: no need to toggle OE
+    readEnable();
     inCycle = data != readData();
-    readDisable();
     counter--;
+    readDisable();
   }
   chipDisable();
   return counter > 0;
@@ -181,6 +183,20 @@ writeBlock:
     // block write failed, retry
     goto writeBlock;
   }
+  dataInputMode();
+  chipEnable();
+  readEnable();
+  for (byte i = 0; i < len; i++) {
+    setAddr(addr + i);
+    if (data[i] != readData()) {
+      Serial.println("bad block, retrying");
+      readDisable();
+      chipDisable();
+      goto writeBlock;
+    }
+  }
+  readDisable();
+  chipDisable();
   digitalWrite(LED_BUILTIN, LOW);
 }
 
@@ -268,17 +284,20 @@ static void printHexByte(byte b) {
 
 static void printHexShort(unsigned short s) {
   Serial.print("0x");
-  Serial.print(hexTable[(byte)(s & 0xf000) >> 12]);
-  Serial.print(hexTable[(byte)(s & 0x0f00) >> 8]);
-  Serial.print(hexTable[(byte)(s & 0x00f0) >> 4]);
-  Serial.print(hexTable[(byte)(s & 0x000f) >> 0]);
+  Serial.print(hexTable[(s & 0xf000) >> 12]);
+  Serial.print(hexTable[(s & 0x0f00) >> 8]);
+  Serial.print(hexTable[(s & 0x00f0) >> 4]);
+  Serial.print(hexTable[(s & 0x000f) >> 0]);
 }
 
-static void readall() {
+static void printBlock(unsigned short a, unsigned short b) {
+  if (a > MAX_ADDR || b > MAX_ADDR) return;
+  unsigned short from = min(a, b);
+  unsigned short to = max(a, b);
   dataInputMode();
   chipEnable();
   readEnable();
-  for (unsigned short i = 0; i < MAX_ADDR; i++) {
+  for (unsigned short i = from; i <= to; i++) {
     if (i % 16 == 0) {
       printHexShort(i);
       Serial.print(": ");
@@ -291,13 +310,16 @@ static void readall() {
       Serial.println();
     }
   }
+  Serial.println();
   readDisable();
   chipDisable();
 }
 
 static char cmd[255];
+static char *cur;
 
 static void prompt() {
+  cur = &cmd[0];
   byte n = 0;
 top:
   n = 0;
@@ -325,102 +347,214 @@ top:
   }
 }
 
+static inline void skipWhite() {
+  while ('\0' != *cur && ' ' == *cur)
+    cur++;
+}
+
+static int readInt() {
+  skipWhite();
+  if (cur[0] == '0' && cur[1] == 'x') {
+    cur += 2;
+    int hex = readHex();
+    return hex;
+  }
+  return readDec();
+}
+
+static int readHex() {
+  static const char *accepted = "0123456789abcdef";
+  int r = 0;
+  bool overflow = false;
+  while (1) {
+    bool found = false;
+    for (int i = 0; i < 16; i++) {
+      if (accepted[i] == *cur) {
+        if (r > (INT_MAX - i) / 16) {
+          overflow = true; // consume the entire number
+        }
+        r = r * 16 + i;
+        cur++;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      return overflow ? -1 : r;
+    }
+  }
+}
+
+static int readDec() {
+  int r = 0;
+  bool overflow = false;
+  while ('\0' != *cur && '0' <= *cur && *cur <= '9') {
+    byte n = *cur - '0';
+    if (r > (INT_MAX - n) / 10) {
+      overflow = true;
+    }
+    r = r * 10 + n;
+    cur++;
+  }
+  return overflow ? -1 : r;
+}
+
+static bool addr() {
+  int addr = readInt();
+  if (addr < 0) {
+    Serial.println("error: address must be positive");
+    return false;
+  }
+  setAddr(addr);
+  return true;
+}
+
+static bool data() {
+  int data = readInt();
+  if (data < 0) {
+    Serial.println("error: data must be positive");
+    return false;
+  }
+  if (data > 255) {
+    Serial.println("error: data must be smaller than 255");
+    return false;
+  }
+  setData(data);
+  return true;
+}
+
+static bool match(char const *s) {
+  int n = strlen(s);
+  bool result = strncmp(s, cur, n) == 0;
+  if (result) {
+    cur += n;
+  }
+  return result;
+}
+
+#define ADDR ((PORT_ADDR_HI << 8) | PORT_ADDR_LO)
+
 static void execute() {
-  String s = String(cmd);
-  s.trim();
-  if (s.startsWith("addr ")) {
-    int addr = s.substring(5).toInt();
-    if (addr < 0) {
-      Serial.println("error: address must be positive");
-      return;
-    }
-    setAddr(addr);
+  skipWhite();
+  if (match("read ")) {
+    if (!addr()) return;
+    Serial.println(readByte(ADDR));
     return;
   }
-  if (s.startsWith("set ")) {
-    int data = s.substring(4).toInt();
-    if (data < 0) {
-      Serial.println("error: data must be positive");
-      return;
-    }
-    if (data > 255) {
-      Serial.println("error: data must be smaller than 255");
-      return;
-    }
-    setData(data);
+  if (match("write ")) {
+    if (!addr()) return;
+    if (!data()) return;
+    writeByte(PORT_DATA, ADDR);
     return;
   }
-  if (s.equals("+chip")) {
+  if (match("block ")) {
+    int a = readInt();
+    int b = readInt();
+    if (a < 0 || b < 0) {
+      Serial.println("block range cannot be negative");
+      return;
+    }
+    printBlock(a, b);
+    return;
+  }
+
+  // low level
+  if (match("addr")) {
+    addr();
+    return;
+  }
+  if (match("data ")) {
+    data();
+    return;
+  }
+  if (match("+chip")) {
     chipEnable();
     return;
   }
-  if (s.equals("+write")) {
-    writeEnable();
-    return;
-  }
-  if (s.equals("+read")) {
-    readEnable();
-    return;
-  }
-  if (s.equals("-chip")) {
+  if (match("-chip")) {
     chipDisable();
     return;
   }
-  if (s.equals("-write")) {
+  if (match("+write")) {
+    writeEnable();
+    return;
+  }
+  if (match("-write")) {
     writeDisable();
     return;
   }
-  if (s.equals("-read")) {
+  if (match("+read")) {
+    readEnable();
+    return;
+  }
+  if (match("-read")) {
     readDisable();
     return;
   }
-  if (s.equals("in")) {
+  if (match("in")) {
     dataInputMode();
     return;
   }
-  if (s.equals("out")) {
+  if (match("out")) {
     dataOutputMode();
     return;
   }
-  if (s.equals("read")) {
+  if (match("get")) {
     Serial.println(readData());
     return;
   }
-  if (s.equals("zero!")) {
-    zero();
-    return;
-  }
-  if (s.equals("unlock")) {
-    unlock();
-    return;
-  }
-  if (s.equals("lock!")) {
+
+  // data protection
+  if (match("lock!")) {
     lock();
     return;
   }
-  if (s.equals("user!")) {
+  if (match("unlock")) {
+    unlock();
+    return;
+  }
+
+  // large block
+  if (match("zero!")) {
+    zero();
+    return;
+  }
+  if (match("user!")) {
     user();
     return;
   }
-  if (s.equals("readall")) {
-    readall();
+  if (match("all")) {
+    printBlock(0, MAX_ADDR);
     return;
   }
-  if (s.equals("help")) {
-    Serial.println("addr <addr> sets the address pins");
-    Serial.println("set <data>  sets the I/O pins");
-    Serial.println("[+|-]chip   enable/disable the chip");
-    Serial.println("[+|-]write  enable/disable writing");
-    Serial.println("[+|-]read   enable/disable reading");
-    Serial.println("in          set MEGA's I/O pins to input (i.e., before reading)");
-    Serial.println("out         set MEGA's I/O pins to output (i.e., before setting data)");
-    Serial.println("read        read the byte at the current address");
-    Serial.println("readall     read the entire EEPROM");
-    Serial.println("zero!       zero-out the EEPROM");
-    Serial.println("unlock      remove software data protection");
-    Serial.println("lock!       set software data protection");
-    Serial.println("user!       write the user data (i.e., your custom code)");
-    Serial.println("help        print this help");
+
+  // misc
+  if (match("help")) {
+    Serial.println("read  <addr>         read a byte");
+    Serial.println("write <addr> <data>  write a byte");
+    Serial.println("block <start> <stop> print a range of bytes");
+    Serial.println();
+    Serial.println("-- low level control --");
+    Serial.println("addr <addr>  set the address pins");
+    Serial.println("data <data>  set the I/O pins");
+    Serial.println("[+|-]chip    enable/disable the chip");
+    Serial.println("[+|-]write   enable/disable writing");
+    Serial.println("[+|-]read    enable/disable reading");
+    Serial.println("in           set MEGA's I/O pins to input (i.e., before reading)");
+    Serial.println("out          set MEGA's I/O pins to output (i.e., before setting data)");
+    Serial.println("get          read the byte at the current address");
+    Serial.println();
+    Serial.println("-- data protection --");
+    Serial.println("unlock  remove software data protection");
+    Serial.println("lock!   set software data protection");
+    Serial.println();
+    Serial.println("-- large block operations --");
+    Serial.println("zero!    zero-out the EEPROM");
+    Serial.println("user!    write the user data (i.e., your custom code)");
+    Serial.println("all      print the entire EEPROM");
+    Serial.println();
+    Serial.println("-- misc --");
+    Serial.println("help:  print this help");
     return;
   }
   Serial.println("error: unknown command -- type 'help' for help");
